2123781c435460ef4a1e43c74b76cf0026a0e711
Jonathan Fuller <fullerjn@gmail.com>
2025-03-31 21:17:24 -0500

    Passing tests for splitter

--- src/splitter.py
+++ src/splitter.py
@@ -1,18 +1,39 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     results = []
     for node in old_nodes:
-        print(node)
-        text = node.text
-        if delimiter not in text:
-            print(False)
-            return
+        if text_type == TextType.TEXT:
+            results.append(node)
         else:
-            print(True)
-            i = 0
-            print(text)
-            print(text.split(delimiter))
-            print(text.find(delimiter))
-            print(text.find(delimiter, 21))
-            print(text.find(delimiter, 32))
+            text = node.text
+            if text.count(delimiter) % 2 != 0:
+                raise Exception("delimiters must be in matching pairs")
+            st = split_text(text, delimiter, text_type)
+            results.extend(st)
+    return results
+
+
+def split_text(text, delimiter, text_type):
+    s = 0
+    len_d = len(delimiter)
+    fmted_text = False
+    results = []
+    while s < len(text):
+        i = text.find(delimiter, s)
+        if i == 0:
+            j = text.find(delimiter, s + len_d)
+            results.append(TextNode(text[s:j+len_d], text_type))
+            s = j + len_d
+        elif i != -1 and not fmted_text:
+            results.append(TextNode(text[s:i], TextType.TEXT))
+            fmted_text = True
+            s = i + 1
+        elif i != -1 and fmted_text:
+            results.append(TextNode(text[s-1:i+len_d], text_type))
+            fmted_text = False
+            s = i + len_d
+        else:
+            results.append(TextNode(text[s:], TextType.TEXT))
+            s = len(text)
+    return results
--- src/test_splitter.py
+++ src/test_splitter.py
@@ -5,6 +5,41 @@ from textnode import TextNode, TextType
 
 class TestSplitter(unittest.TestCase):
 
-    def test_bold(self):
+    def test_code(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+
+        self.assertEqual(
+            [TextNode("This is text with a " , TextType.TEXT), TextNode("`code block`", TextType.CODE), TextNode(" word", TextType.TEXT)],
+            new_nodes
+        )
+
+    def test_starts_bold(self):
+        node = TextNode("**Bold** this is", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+
+        self.assertEqual(
+            [TextNode("**Bold**" , TextType.BOLD), TextNode(" this is", TextType.TEXT)],
+            new_nodes
+        )
+    
+    def test_multiple_italics(self):
+        node = TextNode("This has some _italic text_", TextType.TEXT)
+        node2 = TextNode("And some _more italic_ text", TextType.TEXT)
+        node3 = TextNode("_Italic text_ is everywhere, or hadn't you _heard?_", TextType.TEXT)
+        node4 = TextNode("_How about some_ _back to back_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node, node2, node3, node4], "_", TextType.ITALIC)
+
+        self.assertEqual(
+            [TextNode("This has some ", TextType.TEXT), TextNode("_italic text_", TextType.ITALIC),
+             TextNode("And some ", TextType.TEXT), TextNode("_more italic_", TextType.ITALIC), TextNode(" text", TextType.TEXT),
+             TextNode("_Italic text_", TextType.ITALIC), TextNode(" is everywhere, or hadn't you ", TextType.TEXT), TextNode("_heard?_", TextType.ITALIC),
+             TextNode("_How about some_", TextType.ITALIC), TextNode(" ", TextType.TEXT), TextNode("_back to back_", TextType.ITALIC)],
+             new_nodes
+        )
+    
+    def test_text(self):
+        node = TextNode("Nothing to see here", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], None, TextType.TEXT)
+
+        self.assertEqual([TextNode("Nothing to see here", TextType.TEXT)], new_nodes)
