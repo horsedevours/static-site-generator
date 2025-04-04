77e22894c8afc3b197f474b9f638ef3df5c263f9
Jonathan Fuller <fullerjn@gmail.com>
2025-04-03 20:25:29 -0500

    Added generation for multiple pages

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ import os, shutil
 import htmler
 from textnode import TextNode, TextType
 
-
-print("hello world")
+def extract_title(markdown):
+    lines = markdown.splitlines()
+    for line in lines:
+        if line.startswith("# "):
+            return line.lstrip("# ").rstrip()
 
 def copy_dir_contents(src="static", dest="public"):
     if os.path.exists(dest):
@@ -18,17 +21,22 @@ def copy_dir_contents(src="static", dest="public"):
             dest_path = os.path.join(dest, l)
             copy_dir_contents(src_path, dest_path)
 
-def extract_title(markdown):
-    lines = markdown.splitlines()
-    for line in lines:
-        if line.startswith("# "):
-            return line.lstrip("# ").rstrip()
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    ls = os.listdir(dir_path_content)
+
+    for l in ls:
+        src_path = os.path.join(dir_path_content, l)
+        dest_path = os.path.join(dest_dir_path, l).removesuffix(".md")
+        if os.path.isfile(src_path) and l.endswith(".md"):
+            generate_page(src_path, template_path, dest_path + ".html")
+        elif os.path.isdir(src_path):
+            generate_pages_recursive(src_path, template_path, dest_path)
+
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}...")
     with open(from_path) as f:
         md = f.read()
-    print(md)
     html = htmler.markdown_to_html_node(md).to_html()
     title = extract_title(md)
 
@@ -44,6 +52,6 @@ def generate_page(from_path, template_path, dest_path):
 
 def main():
     copy_dir_contents()
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 main()
